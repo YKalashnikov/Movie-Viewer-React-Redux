b6cee51cd907476065fd367436fc7cba1c4113a9
YKalashnikov <uk2709@gmail.com>
2018-08-14 16:35:21 -0700

    Updates

--- src/actions/movies.js
+++
@@ -1,9 +0,0 @@
-export const addNewMovies = movie => ({
-    type:'ADD_NEW_MOVIE',
-    payload: movie
-})
-export const addNewForm = movie => ({
-    type: 'ADD_NEW_FORM',
-    payload: movie
-})
-
--- src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-import  NewMovies  from "../reducers/NewMovies";
-
-
-const rootReducer = combineReducers({
-    NewMovies
-})
-
-export default rootReducer;
